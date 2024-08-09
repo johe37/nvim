cdcb689c7b163fa72c5a57110e4fe6aa99c70729
johe <johe@example.com>
2024-08-09 22:15:37 +0200

    Lägg till telescope

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,11 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>sf', builtin.find_files, {}) 			-- search files
+vim.keymap.set('n', '<leader>sg', builtin.git_files, {})  			-- search git files
+vim.keymap.set('n', '<leader>gl', builtin.live_grep, {})  			-- grep live
+vim.keymap.set('n', '<leader>gs', builtin.grep_string, {})			-- grep strings
+
+
+-- vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- uses key binding ctrl + p
+-- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+-- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
