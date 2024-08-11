f18a24fbac3bc6c0e48b888dc7d88184fe282c55
johe <johe@example.com>
2024-08-11 22:18:39 +0200

    Använd ctrl+f för goto

--- lua/johe/remap.lua
+++ lua/johe/remap.lua
@@ -6,3 +6,6 @@ vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y") 
 vim.keymap.set("n", "<leader>Y", "\"+Y") 
 vim.keymap.set("n", "<leader>Y", "\"+Y") 
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww goto<CR>")
+
