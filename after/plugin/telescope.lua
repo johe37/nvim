fcf4ed1262b715f22e754f43737da5a27f7c29fc
johe <johe@example.com>
2024-08-11 16:10:40 +0200

    Fixa så att filer som är gitignored kan sökas på

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,14 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>sf', builtin.find_files, {}) 			-- search files
-vim.keymap.set('n', '<leader>sg', builtin.git_files, {})  			-- search git files
-vim.keymap.set('n', '<leader>gl', builtin.live_grep, {})  			-- grep live
-vim.keymap.set('n', '<leader>gs', builtin.grep_string, {})			-- grep strings
+
+vim.keymap.set('n', '<leader>sf', function()			-- search files including git ignored files and hidden files
+  builtin.find_files({
+    no_ignore = true,
+    hidden = true
+  })
+end, {})  							
+vim.keymap.set('n', '<leader>sg', builtin.git_files, {})  	-- search git files
+vim.keymap.set('n', '<leader>gl', builtin.live_grep, {})  	-- grep live
+vim.keymap.set('n', '<leader>gs', builtin.grep_string, {})	-- grep strings
 
 
 -- vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- uses key binding ctrl + p
