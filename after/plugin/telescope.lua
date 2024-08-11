72959c754ca255767333f8f9fea568f5deeb945e
johe <johe@example.com>
2024-08-11 20:17:07 +0200

    Lägg till telescope keyman att grepa på allt

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>sf', function()			-- search files including git ignored files and hidden files
+vim.keymap.set('n', '<leader>sa', function()			-- search files including git ignored files and hidden files
   builtin.find_files({
     no_ignore = true,
     hidden = true
@@ -9,7 +9,21 @@ end, {})
 vim.keymap.set('n', '<leader>sg', builtin.git_files, {})  	-- search git files
 vim.keymap.set('n', '<leader>gl', builtin.live_grep, {})  	-- grep live
 vim.keymap.set('n', '<leader>gs', builtin.grep_string, {})	-- grep strings
-
+vim.keymap.set('n', '<leader>ga', function()			-- grep all
+  builtin.live_grep({
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '--hidden',
+      '--no-ignore'
+    }
+  })
+end, {})
 
 -- vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- uses key binding ctrl + p
 -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
