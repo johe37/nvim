f8217077a59b09cf7021a294d8bfe522c26188a3
johe <johe@example.com>
2024-08-11 20:21:16 +0200

    Lägg till att yank också lägger till i system clipboard

--- lua/johe/remap.lua
+++ lua/johe/remap.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = " " 				-- leader key is space
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) 	-- go back to file explorer
+
+vim.opt.clipboard = "unnamedplus"		-- add yank to system clipboard
+
+
