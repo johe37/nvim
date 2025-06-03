22e64b1ca6f9c7bc357062e8f247fa35c9a81337
johe <johe@example.com>
2025-06-03 22:28:39 +0200

    Change tab to 2 instead of 4

--- lua/johe/set.lua
+++ lua/johe/set.lua
@@ -5,9 +5,9 @@ vim.opt.guicursor = ""              -- fat cursor
 vim.opt.nu = true				    -- line numbers
 
 -- indent 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
