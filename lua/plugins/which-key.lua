f673bb4d2b45b4cb4c00fdcd0bb758db2a2e9599
johe <jonathan.hellgren@basalt.se>
2026-04-16 22:58:24 +0200

    Decrease which-key delay

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,7 +3,7 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
-      delay = 500,
+      delay = 100,
     },
     config = function(_, opts)
       local wk = require("which-key")
