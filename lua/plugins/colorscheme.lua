2461d19679e694f7345e7654420f033ac903d576
johe <56338810+johe37@users.noreply.github.com>
2026-04-16 18:37:07 +0200

    Remove commented out colorscheme code

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,37 +3,8 @@ return {
     "Mofiqul/vscode.nvim",
     priority = 1000,
     config = function()
-      vim.o.background = "dark" -- or "light" if you prefer
+      vim.o.background = "dark"
       vim.cmd("colorscheme vscode")
     end,
   },
 }
---return {
---    {
---      "nyoom-engineering/oxocarbon.nvim",
---      priority = 1000,
---      config = function()
---        vim.o.background = "dark"
---        vim.cmd("colorscheme oxocarbon")
---      end,
---    },
---}
-
--- return {
---   {
---     "folke/tokyonight.nvim",
---     priority = 1000, -- Load before other plugins
---     config = function()
---       require("tokyonight").setup({
---         style = "night", -- Choose from: "storm", "night", "moon", "day"
---         transparent = false,
---         terminal_colors = true,
---         styles = {
---           comments = { italic = false },
---           keywords = { italic = false },
---         },
---       })
---       vim.cmd.colorscheme("tokyonight-night")
---     end,
---   },
--- }
